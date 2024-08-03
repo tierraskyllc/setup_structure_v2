f7cdaba994139e6859414f2f047c91077edde149
TierraSkyLLC <support@tierrasky.com>
2024-08-02 23:31:22 -0400

    added whitenoise

--- src/project/settings.py
+++ src/project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,13 @@ STATICFILES_DIRS = [
 # local cdn
 STATIC_ROOT = BASE_DIR / 'local-cdn'
 
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
